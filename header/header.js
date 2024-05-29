0778eac9ddc5ebf9051b17f1d2e0c0aced029be6
SamuelSarazua <sesarazua@scl.edu.gt>
2024-05-29 11:41:21 -0600

    Actualizacion del header

--- header/header.js
+++ header/header.js
@@ -1,5 +1,8 @@
 let dom_header = document.querySelector(".header")
 
 dom_header.innerHTML=`
-<h1> 🥺Samuel Sarazua🥺 </h1>
+
+    <h1 class ="titulo">Comandos <span class="git">GIT</span></h1>
+    <a href="https://github.com/SamuelSarazua/git_tiempo" class ="btn-github">GitHub</a>
+
 `;
